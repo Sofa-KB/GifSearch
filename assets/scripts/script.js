15f2cb0a4e3105f469507f3953d196ebf237c7e7
SofaKB <hall.b17@gmail.com>
2020-01-18 13:37:06 -0500

    formatted js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,23 +1,21 @@
 var input = prompt("What type of gif would you like to search for?")
-    var gifNumber = prompt("How many would you like to search for?")
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=0aycN4fs7qCEve8DSERNufAG7Mdcs5Ql&q=" + input + "&limit=" + gifNumber;
+var gifNumber = prompt("How many would you like to search for?")
+var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=0aycN4fs7qCEve8DSERNufAG7Mdcs5Ql&q=" + input + "&limit=" + gifNumber;
 
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-      console.log(response);
+document.body.style.backgroundColor = 'black';
 
-      response.data.forEach(function(gif){
+$.ajax({
+  url: queryURL,
+  method: "GET"
 
-        var url = gif.images.downsized_large.url
-        var img = $('<img>')
-        img.attr('src', url)
-        $('#images').append(img)
-
-        document.body.style.backgroundColor = 'black';
-      })
-     
-
-
-    });
+}).then(function(response) {
+  
+  response.data.forEach(function(gif){
+    
+    var url = gif.images.downsized_large.url
+    var img = $('<img>')
+    
+    img.attr('src', url)
+    $('#images').append(img)
+  })
+});
