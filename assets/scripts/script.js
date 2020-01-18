4a6e0e38a92d06f6234a04c96534d9b3ee30ef62
Brandon Hall <54915861+Sofa-KB@users.noreply.github.com>
2020-01-18 12:50:38 -0500

    Updated to https

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,6 @@
 var input = prompt("What type of gif would you like to search for?")
     var gifNumber = prompt("How many would you like to search for?")
-    var queryURL = "http://api.giphy.com/v1/gifs/search?api_key=0aycN4fs7qCEve8DSERNufAG7Mdcs5Ql&q=" + input + "&limit=" + gifNumber;
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=0aycN4fs7qCEve8DSERNufAG7Mdcs5Ql&q=" + input + "&limit=" + gifNumber;
 
     $.ajax({
       url: queryURL,
